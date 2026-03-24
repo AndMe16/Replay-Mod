c85bde191136e6054f604e76db519fe6cdc51785
AndMe <afmendez99@outlook.com>
2026-03-24 13:41:46 +0100

    initial commit

---
+++ Replay Mod/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+using ReplayMod;
+
+namespace Replay_Mod
+{ // will be replaced by assemblyName if desired
+    [BepInPlugin("com.andme.replaymod", "ReplayMod", MyPluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource logger;
+        private Harmony harmony;
+
+        public static Plugin Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = this;
+            logger = Logger;
+
+            harmony = new Harmony("com.andme.replaymod");
+            harmony.PatchAll();
+
+            logger.LogInfo("Plugin com.andme.replaymod is loaded!");
+        }
+
+        private void OnDestroy()
+        {
+            harmony?.UnpatchSelf();
+            harmony = null;
+        }
+    }
+}
